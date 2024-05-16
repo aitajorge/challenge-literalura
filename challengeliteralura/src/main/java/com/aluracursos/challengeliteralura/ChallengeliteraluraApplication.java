8d3c1f913f985a46e834cee92d5ea121e4449e22
Jorge Aita <aitajorge@yahoo.com.ar>
2024-05-16 19:06:45 -0300

    primer: README, entorno Java y Spring más API

---
+++ challengeliteralura/src/main/java/com/aluracursos/challengeliteralura/ChallengeliteraluraApplication.java
@@ -0,0 +1,22 @@
+package com.aluracursos.challengeliteralura;
+
+import com.aluracursos.challengeliteralura.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeliteraluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChallengeliteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://gutendex.com/books/");
+		System.out.println(json);
+	}
+}
